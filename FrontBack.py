81c3ba552ec8979aff66f29166997ce0a94918b1
Demoleas715 <Demoleas715@gmail.com>
2015-09-14 20:29:47 -0400

    reverses first and last letter of string

--- FrontBack.py
+++ FrontBack.py
@@ -1,11 +1,5 @@
 """Front Back"""
-"""var = input("What's your word?")
+
+var = input("What's your word?")
 midVar = var[1:len(var)-1]
-print(var[len(var)-1] + midVar + var[0])"""
-
-def front_back(var):
-	if len(var) <=1:
-		return var
-
-	midVar = var[1:len(var)-1]
-	return var[1:len(var)-1] + midVar +var[0]
+print(var[len(var)-1] + midVar + var[0])
